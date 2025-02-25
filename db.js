3c50bb7abbc45ff2878ae69c2c6ee9b01b9a1e1a
unknown <gwenpena21@gmail.com>
2025-02-25 18:30:32 +0800

    Fixed route

--- db.js
+++ db.js
@@ -5,7 +5,11 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI || "");
+    
+    await mongoose.connect(process.env.MONGO_URI || "", {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Mongo DB connected!");
   } catch (error) {
     console.error("MongoDB connection error:", error);
--- routes/hotels.route.js
+++ routes/hotels.route.js
@@ -1,16 +1,17 @@
 import { Router } from "express";
 import Hotel from "../models/hotel.model.js";
+import connectDB from "../db.js";
 
 const router = Router();
 
 router.post("/", async (req, res) => {
+  await connectDB();
   const hotel = new Hotel(req.body);
   try {
     const savedHotel = await hotel.save();
     res.status(200).json(savedHotel);
   } catch (error) {
-    console.log(error)
-    res.status(500).json(`Something went wrong.v${error}`);
+    res.status(500).json(`Something went wrong.${error}`);
   }
 });
 
