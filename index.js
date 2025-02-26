8cee1b1201f7577d678f555e1b97e27e8001b823
unknown <gwenpena21@gmail.com>
2025-02-26 19:52:19 +0800

    Added swagger

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerFile), {
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerFile, {
+  
     customCss:
         '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
     customCssUrl: CSS_URL,
-}
-));
+})
+);
 app.use("/auth", authRoutes);
 app.use("/hotels", hotelsRoutes);
 
