82e165690f01fc3095f86bf9bd37a30ce779060a
unknown <gwenpena21@gmail.com>
2025-02-26 19:51:05 +0800

    Added swagger

--- index.js
+++ index.js
@@ -8,13 +8,19 @@ import swaggerUi from "swagger-ui-express";
 import swaggerFile from './swagger-output.json' with { type: "json" };
 
 dotenv.config();
+const CSS_URL = "https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.1.0/swagger-ui.min.css"
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use("/", swaggerUi.serve, swaggerUi.setup(swaggerFile), {
+    customCss:
+        '.swagger-ui .opblock .opblock-summary-path-description-wrapper { align-items: center; display: flex; flex-wrap: wrap; gap: 0 10px; padding: 0 10px; width: 100%; }',
+    customCssUrl: CSS_URL,
+}
+));
 app.use("/auth", authRoutes);
 app.use("/hotels", hotelsRoutes);
 
