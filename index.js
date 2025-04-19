15809985239938576555b44799adf003a788a92e
unknown <gwenpena21@gmail.com>
2025-04-19 17:55:52 +0800

    Fixed cors

--- index.js
+++ index.js
@@ -20,15 +20,10 @@ const allowedOrigins = [
   'http://localhost:5173'
 ]
 
+
 app.use(
   cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
+    origin: allowedOrigins,
     credentials: true,
   })
 );
