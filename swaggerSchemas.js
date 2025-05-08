dbceaa4534dc098b9c7626659178019efa4e18b8
unknown <gwenpena21@gmail.com>
2025-05-08 18:02:53 +0800

    Changed swagger schema'

--- swaggerSchemas.js
+++ swaggerSchemas.js
@@ -1,6 +1,7 @@
 import { Hotel } from "./models/hotel.model.js";
 import { User, UserLogin } from "./models/user.model.js";
 import m2s from "mongoose-to-swagger";
+import { UserBooking } from "./models/userBooking.model.js";
 
 export default {
   user: m2s(User, { omitMongooseInternals: true, omitFields: ["_id"] }),
@@ -9,4 +10,5 @@ export default {
     omitFields: ["_id"],
   }),
   hotel: m2s(Hotel, { omitMongooseInternals: true, omitFields: ["_id"] }),
+  hotel: m2s(UserBooking, { omitMongooseInternals: true, omitFields: ["_id"] }),
 };
